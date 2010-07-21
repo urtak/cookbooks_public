40f8482e9c0129ab4753a0f6a57e5c5024b17271
Aaron Gibralter <aaron.gibralter@gmail.com>
2010-07-21 19:50:57 -0400

    A missing :type => :hash in rs_utils?

--- cookbooks/rs_utils/metadata.rb
+++ cookbooks/rs_utils/metadata.rb
@@ -15,6 +15,10 @@ recipe "rs_utils::setup_timezone", "Sets system timezone."
 recipe "rs_utils::install_mysql_collectd_plugin", "Install mysql collectd plugin"
 recipe "rs_utils::install_file_stats_collectd_plugin", "Install file-stats.rb collectd plugin.  This is used for mysql binary backup alerting."
 
+attribute "rs_utils",
+  :display_name => "RS Utils Settings",
+  :type => "hash"
+
 attribute "rs_utils/timezone",
   :display_name => "Timezone",
   :description => "Sets the system time to the timezone of the specified input, which must be a valid zoneinfo/tz database entry.  If the input is 'unset' the timezone will use the 'localtime' that's defined in your RightScale account under Settings -> User -> Preferences tab.  You can find a list of valid examples from the timezone pulldown bar in the Preferences tab. The server will not be updated for daylight savings time.  Ex: US/Pacific, US/Eastern",
