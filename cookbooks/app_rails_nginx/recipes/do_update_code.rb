0058c3d9d8ef2ab24aec129d4239e10102bd8c85
Aaron Gibralter <aaron.gibralter@gmail.com>
2010-07-23 13:48:17 -0400

    Since the repo_git LWRP can't `include_recipe "git"` do it here.

--- cookbooks/app_rails_nginx/recipes/do_update_code.rb
+++ cookbooks/app_rails_nginx/recipes/do_update_code.rb
@@ -1,6 +1,9 @@
 # Raise trouble for required attributes.
 raise "missing rails/code/url" if "#{node[:rails][:code][:url]}".empty?
 
+# include the public recipe to install git
+include_recipe "git::default"
+
 # grab application source from remote repository
 repo "Get Repository" do
   url node[:rails][:code][:url]
